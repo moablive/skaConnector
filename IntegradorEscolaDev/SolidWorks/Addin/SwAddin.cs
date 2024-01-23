e4d107f8984abe6b21be07578f92e3e03f101813
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-01-23 17:29:06 -0300

    init

---
+++ IntegradorEscolaDev/SolidWorks/Addin/SwAddin.cs
@@ -0,0 +1,530 @@
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using SolidWorks.Interop.swpublished;
+
+using SolidWorksTools;
+using SolidWorksTools.File;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace IntegradorEscolaDev
+{
+    /// <summary>
+    /// Summary description for IntegradorSW.
+    /// </summary>
+    //[Guid("907F5A5B-0998-4EB9-96AC-A6F120CEE037"), ComVisible(true)]
+    //[Guid("7D8BE380-8B21-4C24-875A-2491451673FB")]
+    [Guid("8E731648-369D-41FD-AD2E-63FA2D9930D9"), ComVisible(true)]
+    [SwAddin(
+        Description = "SKACONNECTOR SOLIDWORKS ESCOLA DEV",
+        Title = "SKACONNECTOR ESCOLA DEV",
+        LoadAtStartup = true
+        )]
+    public class SwAddin : ISwAddin
+    {
+        #region Local Variables
+        ISldWorks iSwApp = null;
+        ICommandManager iCmdMgr = null;
+        int addinID = 0;
+        BitmapHandler iBmp;
+        int registerID;
+
+        public const int mainCmdGroupID = 5;
+        public const int mainItemID1 = 0;
+        public const int mainItemID2 = 1;
+        public const int mainItemID3 = 2;
+        public const int flyoutGroupID = 91;
+
+        TaskpaneView tpv = null;
+        public InterfaceSW TaskPaneUserControl;
+
+        string[] mainIcons = new string[6];
+        string[] icons = new string[6];
+
+        #region Event Handler Variables
+        Hashtable openDocs = new Hashtable();
+        SolidWorks.Interop.sldworks.SldWorks SwEventPtr = null;
+        #endregion
+
+        #region Property Manager Variables
+        public UserPMPage ppage = null;
+        #endregion
+
+
+        // Public Properties
+        public ISldWorks SwApp
+        {
+            get { return iSwApp; }
+        }
+        public ICommandManager CmdMgr
+        {
+            get { return iCmdMgr; }
+        }
+
+        public Hashtable OpenDocs
+        {
+            get { return openDocs; }
+        }
+
+        #endregion
+
+        #region SolidWorks Registration
+        [ComRegisterFunctionAttribute]
+        public static void RegisterFunction(Type t)
+        {
+            #region Get Custom Attribute: SwAddinAttribute
+            SwAddinAttribute SWattr = null;
+            Type type = typeof(SwAddin);
+
+            foreach (System.Attribute attr in type.GetCustomAttributes(false))
+            {
+                if (attr is SwAddinAttribute)
+                {
+                    SWattr = attr as SwAddinAttribute;
+                    break;
+                }
+            }
+
+            #endregion
+
+            try
+            {
+                Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+                Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
+                Microsoft.Win32.RegistryKey addinkey = hklm.CreateSubKey(keyname);
+                addinkey.SetValue(null, 0);
+
+                addinkey.SetValue("Description", SWattr.Description);
+                addinkey.SetValue("Title", SWattr.Title);
+
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
+                addinkey = hkcu.CreateSubKey(keyname);
+                addinkey.SetValue(null, Convert.ToInt32(SWattr.LoadAtStartup), Microsoft.Win32.RegistryValueKind.DWord);
+            }
+            catch (System.NullReferenceException nl)
+            {
+                Console.WriteLine("There was a problem registering this dll: SWattr is null. \n\"" + nl.Message + "\"");
+                System.Windows.Forms.MessageBox.Show("There was a problem registering this dll: SWattr is null.\n\"" + nl.Message + "\"");
+            }
+
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                System.Windows.Forms.MessageBox.Show("There was a problem registering the function: \n\"" + e.Message + "\"");
+            }
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void UnregisterFunction(Type t)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+                Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
+                hklm.DeleteSubKey(keyname);
+
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
+                hkcu.DeleteSubKey(keyname);
+            }
+            catch (System.NullReferenceException nl)
+            {
+                Console.WriteLine("There was a problem unregistering this dll: " + nl.Message);
+                System.Windows.Forms.MessageBox.Show("There was a problem unregistering this dll: \n\"" + nl.Message + "\"");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("There was a problem unregistering this dll: " + e.Message);
+                System.Windows.Forms.MessageBox.Show("There was a problem unregistering this dll: \n\"" + e.Message + "\"");
+            }
+        }
+
+        #endregion
+
+        #region ISwAddin Implementation
+        public SwAddin()
+        {
+        }
+
+        public bool ConnectToSW(object ThisSW, int cookie)
+        {
+            iSwApp = (ISldWorks)ThisSW;
+            addinID = cookie;
+
+            //Setup callbacks
+            iSwApp.SetAddinCallbackInfo(0, this, addinID);
+
+            string CaminhoIcone = System.Reflection.Assembly.GetAssembly(this.GetType()).Location.ToUpper().Replace("INTEGRADORESCOLADEV.DLL", "") + "SKA.ico";
+            tpv = iSwApp.CreateTaskpaneView2(CaminhoIcone, "SKAConnector SolidWorks Escola DEV");
+            tpv.AddControl("IntegradorEscolaDev.InterfaceSW", "");
+
+            TaskPaneUserControl = (InterfaceSW)this.tpv.GetControl();
+
+            #region Setup the Command Manager
+            iCmdMgr = iSwApp.GetCommandManager(cookie);
+            //AddCommandMgr();
+            #endregion
+
+            #region Setup the Event Handlers
+            SwEventPtr = (SolidWorks.Interop.sldworks.SldWorks)iSwApp;
+            openDocs = new Hashtable();
+            AttachEventHandlers();
+            #endregion
+
+            #region Setup Sample Property Manager
+            AddPMP();
+            #endregion
+
+            return true;
+        }
+
+        public bool DisconnectFromSW()
+        {
+            RemoveCommandMgr();
+            RemovePMP();
+            DetachEventHandlers();
+
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(iCmdMgr);
+            iCmdMgr = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(iSwApp);
+            iSwApp = null;
+            //The addin _must_ call GC.Collect() here in order to retrieve all managed code pointers 
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            return true;
+        }
+        #endregion
+
+        #region UI Methods
+
+
+        public void RemoveCommandMgr()
+        {
+            iBmp.Dispose();
+
+            iCmdMgr.RemoveCommandGroup(mainCmdGroupID);
+            iCmdMgr.RemoveFlyoutGroup(flyoutGroupID);
+        }
+
+        public bool CompareIDs(int[] storedIDs, int[] addinIDs)
+        {
+            List<int> storedList = new List<int>(storedIDs);
+            List<int> addinList = new List<int>(addinIDs);
+
+            addinList.Sort();
+            storedList.Sort();
+
+            if (addinList.Count != storedList.Count)
+            {
+                return false;
+            }
+            else
+            {
+
+                for (int i = 0; i < addinList.Count; i++)
+                {
+                    if (addinList[i] != storedList[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Boolean AddPMP()
+        {
+            ppage = new UserPMPage(this);
+            return true;
+        }
+
+        public Boolean RemovePMP()
+        {
+            ppage = null;
+            return true;
+        }
+
+        #endregion
+
+        #region UI Callbacks
+        //public void CreateCube()
+        //{
+        //    //make sure we have a part open
+        //    string partTemplate = iSwApp.GetUserPreferenceStringValue((int)swUserPreferenceStringValue_e.swDefaultTemplatePart);
+        //    if ((partTemplate != null) && (partTemplate != ""))
+        //    {
+        //        IModelDoc2 modDoc = (IModelDoc2)iSwApp.NewDocument(partTemplate, (int)swDwgPaperSizes_e.swDwgPaperA2size, 0.0, 0.0);
+
+        //        modDoc.InsertSketch2(true);
+        //        modDoc.SketchRectangle(0, 0, 0, .1, .1, .1, false);
+        //        //Extrude the sketch
+        //        IFeatureManager featMan = modDoc.FeatureManager;
+        //        featMan.FeatureExtrusion(true,
+        //            false, false,
+        //            (int)swEndConditions_e.swEndCondBlind, (int)swEndConditions_e.swEndCondBlind,
+        //            0.1, 0.0,
+        //            false, false,
+        //            false, false,
+        //            0.0, 0.0,
+        //            false, false,
+        //            false, false,
+        //            true,
+        //            false, false);
+        //    }
+        //    else
+        //    {
+        //        System.Windows.Forms.MessageBox.Show("There is no part template available. Please check your options and make sure there is a part template selected, or select a new part template.");
+        //    }
+        //}
+
+        public void PopupCallbackFunction()
+        {
+            bool bRet;
+
+            bRet = iSwApp.ShowThirdPartyPopupMenu(registerID, 500, 500);
+        }
+
+        public int PopupEnable()
+        {
+            if (iSwApp.ActiveDoc == null)
+                return 0;
+            else
+                return 1;
+        }
+
+        public void TestCallback()
+        {
+            Debug.Print("Test Callback, CSharp");
+        }
+
+        public int EnableTest()
+        {
+            if (iSwApp.ActiveDoc == null)
+                return 0;
+            else
+                return 1;
+        }
+
+        public void ShowPMP()
+        {
+            if (ppage != null)
+                ppage.Show();
+        }
+
+        public int EnablePMP()
+        {
+            if (iSwApp.ActiveDoc != null)
+                return 1;
+            else
+                return 0;
+        }
+
+        public void FlyoutCallback()
+        {
+            FlyoutGroup flyGroup = iCmdMgr.GetFlyoutGroup(flyoutGroupID);
+            flyGroup.RemoveAllCommandItems();
+
+            flyGroup.AddCommandItem(System.DateTime.Now.ToLongTimeString(), "test", 0, "FlyoutCommandItem1", "FlyoutEnableCommandItem1");
+
+        }
+        public int FlyoutEnable()
+        {
+            return 1;
+        }
+
+        public void FlyoutCommandItem1()
+        {
+            iSwApp.SendMsgToUser("Flyout command 1");
+        }
+
+        public int FlyoutEnableCommandItem1()
+        {
+            return 1;
+        }
+        #endregion
+
+        #region Event Methods
+        public bool AttachEventHandlers()
+        {
+            AttachSwEvents();
+            //Listen for events on all currently open docs
+            AttachEventsToAllDocuments();
+            return true;
+        }
+
+        private bool AttachSwEvents()
+        {
+            try
+            {
+                SwEventPtr.ActiveDocChangeNotify += new DSldWorksEvents_ActiveDocChangeNotifyEventHandler(OnDocChange);
+                SwEventPtr.DocumentLoadNotify2 += new DSldWorksEvents_DocumentLoadNotify2EventHandler(OnDocLoad);
+                SwEventPtr.FileNewNotify2 += new DSldWorksEvents_FileNewNotify2EventHandler(OnFileNew);
+                SwEventPtr.ActiveModelDocChangeNotify += new DSldWorksEvents_ActiveModelDocChangeNotifyEventHandler(OnModelChange);
+                SwEventPtr.FileOpenPostNotify += new DSldWorksEvents_FileOpenPostNotifyEventHandler(FileOpenPostNotify);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+
+
+        private bool DetachSwEvents()
+        {
+            try
+            {
+                SwEventPtr.ActiveDocChangeNotify -= new DSldWorksEvents_ActiveDocChangeNotifyEventHandler(OnDocChange);
+                SwEventPtr.DocumentLoadNotify2 -= new DSldWorksEvents_DocumentLoadNotify2EventHandler(OnDocLoad);
+                SwEventPtr.FileNewNotify2 -= new DSldWorksEvents_FileNewNotify2EventHandler(OnFileNew);
+                SwEventPtr.ActiveModelDocChangeNotify -= new DSldWorksEvents_ActiveModelDocChangeNotifyEventHandler(OnModelChange);
+                SwEventPtr.FileOpenPostNotify -= new DSldWorksEvents_FileOpenPostNotifyEventHandler(FileOpenPostNotify);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
+        public void AttachEventsToAllDocuments()
+        {
+            ModelDoc2 modDoc = (ModelDoc2)iSwApp.GetFirstDocument();
+            while (modDoc != null)
+            {
+                if (!openDocs.Contains(modDoc))
+                {
+                    AttachModelDocEventHandler(modDoc);
+                }
+                else if (openDocs.Contains(modDoc))
+                {
+                    bool connected = false;
+                    DocumentEventHandler docHandler = (DocumentEventHandler)openDocs[modDoc];
+                    if (docHandler != null)
+                    {
+                        connected = docHandler.ConnectModelViews();
+                    }
+                }
+
+                modDoc = (ModelDoc2)modDoc.GetNext();
+            }
+        }
+
+        public bool AttachModelDocEventHandler(ModelDoc2 modDoc)
+        {
+            if (modDoc == null)
+                return false;
+
+            DocumentEventHandler docHandler = null;
+
+            if (!openDocs.Contains(modDoc))
+            {
+                switch (modDoc.GetType())
+                {
+                    case (int)swDocumentTypes_e.swDocPART:
+                        {
+                            docHandler = new PartEventHandler(modDoc, this);
+                            break;
+                        }
+                    case (int)swDocumentTypes_e.swDocASSEMBLY:
+                        {
+                            docHandler = new AssemblyEventHandler(modDoc, this);
+                            break;
+                        }
+                    case (int)swDocumentTypes_e.swDocDRAWING:
+                        {
+                            docHandler = new DrawingEventHandler(modDoc, this);
+                            break;
+                        }
+                    default:
+                        {
+                            return false; //Unsupported document type
+                        }
+                }
+                docHandler.AttachEventHandlers();
+                openDocs.Add(modDoc, docHandler);
+            }
+            return true;
+        }
+
+        public bool DetachModelEventHandler(ModelDoc2 modDoc)
+        {
+            DocumentEventHandler docHandler;
+            docHandler = (DocumentEventHandler)openDocs[modDoc];
+            openDocs.Remove(modDoc);
+            modDoc = null;
+            docHandler = null;
+            return true;
+        }
+
+        public bool DetachEventHandlers()
+        {
+            DetachSwEvents();
+
+            //Close events on all currently open docs
+            DocumentEventHandler docHandler;
+            int numKeys = openDocs.Count;
+            object[] keys = new Object[numKeys];
+
+            //Remove all document event handlers
+            openDocs.Keys.CopyTo(keys, 0);
+            foreach (ModelDoc2 key in keys)
+            {
+                docHandler = (DocumentEventHandler)openDocs[key];
+                docHandler.DetachEventHandlers(); //This also removes the pair from the hash
+                docHandler = null;
+            }
+            return true;
+        }
+        #endregion
+
+        #region Event Handlers
+        //Events
+        public int OnDocChange()
+        {
+            return 0;
+        }
+
+        public int OnDocLoad(string docTitle, string docPath)
+        {
+            return 0;
+        }
+
+        int FileOpenPostNotify(string FileName)
+        {
+            AttachEventsToAllDocuments();
+            return 0;
+        }
+
+        public int OnFileNew(object newDoc, int docType, string templateName)
+        {
+            AttachEventsToAllDocuments();
+            return 0;
+        }
+
+        public int OnModelChange()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+
+}
